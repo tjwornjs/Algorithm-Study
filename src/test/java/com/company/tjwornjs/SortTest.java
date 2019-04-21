882abfd4004f28f1b1aeffc6080c5f33b1cad517
jaegwon.seo <jaegwon.seo@navercorp.com>
2019-04-21 22:27:33 +0900

    선택 정렬 추가

--- src/test/java/com/company/tjwornjs/SortTest.java
+++ src/test/java/com/company/tjwornjs/SortTest.java
@@ -2,6 +2,7 @@ package com.company.tjwornjs;
 
 import com.company.tjwornjs.sort.InsertionSort;
 import com.company.tjwornjs.sort.MergeSort;
+import com.company.tjwornjs.sort.SelectionSort;
 import com.company.tjwornjs.sort.Sort;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,8 +10,8 @@ import org.junit.Test;
 import java.util.Arrays;
 
 public class SortTest {
-    int[] unSortedList = {5, 3, 6, 9, 1, 50, 8};
-    int[] sortedList = {1, 3, 5, 6, 8, 9, 50};
+    private final int[] unSortedList = {5, 3, 6, 9, 1, 50, 8};
+    private final int[] sortedList = {1, 3, 5, 6, 8, 9, 50};
 
     @Test
     public void insertSortTest() {
@@ -27,4 +28,13 @@ public class SortTest {
         sort.sortArray(list);
         Assert.assertArrayEquals(sortedList, list);
     }
+
+    @Test
+    public void SelectionSortTest() {
+        Sort sort = new SelectionSort();
+        int[] list = Arrays.copyOf(unSortedList, unSortedList.length);
+        sort.sortArray(list);
+        Assert.assertArrayEquals(sortedList, list);
+    }
+
 }
